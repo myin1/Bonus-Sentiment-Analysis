e8035de9b239e25c9929aaebaf15846768703ac4
mimido8 <43546057+mimido8@users.noreply.github.com>
2019-04-23 12:41:39 -0400

    Create backbone.c

---
+++ backbone.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+extern char posWords[idk];
+extern char negWords[idk];
+
+//declaring variables
+int pos = 0;
+int neg = 0;
+int polar = 0;
+
+int main(void){
+return 0;
+}
+
+void countPos(char reviews[idk1][idk2]){
+}
+
+void countNeg(char reviews[idk1][idk2]){
+}
+
+int calcPol(int pos, int neg){
+
+}
+
